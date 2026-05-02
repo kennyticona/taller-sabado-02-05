187ebced80c76947423c4e7e2939523a8149dde3
Kenny <kennyticonah@gmail.com>
2026-05-02 15:06:22 +0000

    Mi primer commit desde cloud9

---
+++ prueba3.py
@@ -0,0 +1,10 @@
+def main():
+    mes_del_año= int(input(" Inserte mes del año"))
+    
+  i=0
+    while i = 2 
+    else i = 6 
+    else i = 10
+        print ("vender acciones", i)
+        
+main()
